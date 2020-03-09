c46d4b8081b2d16aa98be05a222f00df529a6332
meganwolf <meganpwolf@gmail.com>
2020-03-09 17:19:27 -0400

    updated background and sizes

--- client/src/views/PatientLogin.js
+++ client/src/views/PatientLogin.js
@@ -1,44 +1,61 @@
   
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, TextField, makeStyles, Grid} from '@material-ui/core';
+import { Button, TextField, makeStyles, Grid, StylesProvider} from '@material-ui/core';
+import Background from '../assets/background.png';
+
 
 const useStyles = makeStyles(theme => ({
-  
+  paperContainer: {
+    backgroundImage: `url(${Background})`
+    
+  },
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
       width: 300,
+      justify: 'center',
+      background: 'white'
+      
     },
   },
   asd: {
-    background: theme.background,
+    
+    background: 'orange',
     fontSize: 16,
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'blue',
     height: 48,
-    padding: '0 120px', 
+    padding: '0 100px', 
+    justifyContent: 'right',
+    
   },
   user: {
     
   },
+
 }));
 
 const PatientLogin = () => {
   const classes = useStyles();
   
         return (
+        //<div className = "background">
+        //<div className="Wrapper">
 
-                 <div>
-                         
+                // <div className={classes.root}>
+                //         This is the login page <br />
+                    <div className={classes.paperContainer}>
                         <Grid  container
                         spacing={0}
                         direction="column"
                         alignItems="center"
                         justify="center"
-                        style={{ minHeight: '100vh' }}  >
-                          <h3>Login</h3>
+                        style={{ minHeight: '100vh' }
+
+                        }  >
+                          
                       <form className={classes.root} noValidate autoComplete="off">
                     
                         <TextField className={classes.user} id="outlined-basic" label="Username" variant="outlined" />
@@ -48,7 +65,7 @@ const PatientLogin = () => {
                         <Button className={classes.asd} component={Link} to="/patientview" variant="outlined" color="primary">Login</Button>
                       </form>
                       </Grid>
-          // </div>
+                  </div>
          
         )
                         
