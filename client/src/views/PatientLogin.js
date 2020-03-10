0c2995e05c1547aecf3e1aad07a51e14690f07a2
meganwolf <meganpwolf@gmail.com>
2020-03-09 20:45:41 -0400

    updated to use material ui template

--- client/src/views/PatientLogin.js
+++ client/src/views/PatientLogin.js
@@ -1,90 +1,119 @@
-  
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Links from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import { Link } from 'react-router-dom';
-import { Button, TextField, makeStyles, Grid, StylesProvider, Typography} from '@material-ui/core';
-import Background from '../assets/background.png';
 
+// Material UI Template used to create login forms design
+// https://github.com/mui-org/material-ui/tree/master/docs/src/pages/getting-started/templates/sign-in
 
-const useStyles = makeStyles(theme => ({
-  paperContainer: {
-     backgroundImage: `url(${Background})`,
-    // width: '100%',
-    //  height: '100%',
-    //  backgroundSize: 'contain',
-    //______________________________
-     backgroundPosition: 'cover',
-    backgroundPosition: 'center',
-    width: `calc(94vw + 175px)`,
-    height: 997,
-    margin: -120,
-    padding: 30,
-  },
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-      width: 300,
-      //justify: 'center',
-      background: 'white'
-      
-    },
-  },
-  loginbutton: {
-    background: 'orange',
-    fontSize: 16,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'blue',
-    height: 48,
-    padding: '0 100px', 
-    justifyContent: 'right',
-    
-  },
-  user: {
-    color: 'black',
-  },
-  loginText:{
-    color: 'orange',
-  }
-
-}));
-
-const PatientLogin = () => {
-  const classes = useStyles();
-  
-        return (
-        //<div className = "background">
-        //<div className="Wrapper">
-
-                 //<div>
-                  //       This is the login page <br />
-                    <div className={classes.paperContainer}>
-                        
-                      <Grid  container
-                        spacing={0}
-                        direction="column"
-                        alignItems="center"
-                        justify="center"
-                        style={{ minHeight: '100vh' }
-
-                        }  >
-                          <Typography className={classes.loginText} component="h1" variant="h4">
-                            Login
-                          </Typography>
-                      <form className={classes.root} noValidate autoComplete="off">
-                    
-                        <TextField className={classes.user} color='white' id="outlined-basic" label="Username" variant="outlined" />
-                        <div>
-                        <TextField id="outlined-basic" label="Password" variant="outlined" />
-                        </div>
-                        <br></br>
-                        <Button className={classes.loginbutton} component={Link} to="/patientview" variant="outlined" color="primary">Login</Button>
-                      </form>
-                      </Grid>
-                  </div>
-                  //</div>
-         
-        )
-                        
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Links color="inherit" href="https://material-ui.com/">
+        UF Neurosurgery Department
+      </Links>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default PatientLogin;
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: "orange",
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          UF Neurosurgery Patient Portal
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            component={Link} to="/patientview"
+
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Links href="#" variant="body2">
+                Forgot password?
+              </Links>
+            </Grid>
+            <Grid item>
+              <Links href="tel:9047088717" variant="body2">
+                {"Don't have an account? Call Your Doctor"}
+              </Links>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
