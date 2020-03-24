17c0a317db4f0cddbc65aad5662e71d829418fc4
Rohil Tuli <rohil.tuli@gmail.com>
2020-03-24 13:03:26 -0400

    Worked on storing distance/time to clinic in database, adjusted schemas some

--- server/controllers/databaseController.js
+++ server/controllers/databaseController.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const Example = require('../models/examples.server.model.js')
 const Appointment = require('../models/AppointmentModel.js')
 const Patient = require('../models/PatientModel.js')
+const config = require('../config/config.js')
+const axios = require('axios')
 
 exports.addPatient = async (req, res) => {
     Patient.create({
@@ -16,7 +18,7 @@ exports.addPatient = async (req, res) => {
 
 exports.addAppointment = async (req, res) => {
     Appointment.create({
-        name : req.query.name,
+        patientId : req.query.patientId,
         date : req.query.date,
     }, (err) => {
         if (err) throw err;
@@ -25,8 +27,17 @@ exports.addAppointment = async (req, res) => {
 }
 
 exports.getPatients = async (req, res) => {
-    Patient.find({}, (err, docs) => {
+    Patient.find({}, async (err, docs) => {
         if (err) throw err;
+        docs.forEach(async (doc) => {
+            if (!doc.distanceToClinic || !doc.timeToClinic) {
+                const origin = doc.address;
+                const response = await axios.get('https://maps.googleapis.com/maps/api/directions/json?key=' + config.directions.key + '&origin=' + origin + '&destination=Parking Garage 10 Newell Dr, Gainesville, FL 32603');
+                doc.distanceToClinic = response.data.routes[0].legs[0].distance.text;
+                doc.timeToClinic = response.data.routes[0].legs[0].duration.text;
+                doc.save();
+            }
+        });
         res.send(docs);
     });
 }
