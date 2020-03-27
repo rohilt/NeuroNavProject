d338f42afc2f063f9c1eab4674898e8593f5d93e
meganwolf <meganpwolf@gmail.com>
2020-03-26 23:54:00 -0400

    moved homepage to album cards

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,21 +1,23 @@
-import React from 'react';
+
 import NavBar from "./NavBar2"
 
 import './Home.css';
 
-import Avatar from '@material-ui/core/Avatar';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Links from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Links from '@material-ui/core/Link';
 import { Link } from 'react-router-dom';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
@@ -40,27 +42,44 @@ const theme = createMuiTheme({
   },
 });
 
-const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1
-    },
-    menuButton: {
-      marginRight: theme.spacing(-100),
-    },
-    palette: {
-        primary: blue,
-        secondary: {
-          main: '#f44336',
-        },
-    },
-  
-   
-    title: {
-      
-      flexGrow: 1
-    },
-    
-  }));
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+  palette: {
+    primary: blue,
+    secondary: {
+      main: '#f44336',
+    }
+  }
+}));
 
   function Copyright() {
     return (
@@ -76,71 +95,110 @@ const useStyles = makeStyles(theme => ({
     );
   }
 
-function Home() {
-    const classes = useStyles();
-    
-    return (
-        <MuiThemeProvider theme={theme}>
-        <div className="App" >
-            <NavBar />
-            <header className="App-header" >
-                
-                 
-                
-                
-            </header>
-            <div>
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+export function Home() {
+  const classes = useStyles();
 
-<Container component="main" maxWidth="xs">
-  <CssBaseline />
-  <div className={classes.paper}>
+  return (
+    <MuiThemeProvider theme={theme}>
+    <NavBar />
+    <React.Fragment>
+      <CssBaseline />
 
-
-
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        color="secondary"
-        className={classes.submit}
-        component={Link} to="/directions"
-
-      >
-        Click for Directions
-       
-      </Button>
-
-       
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        color="secondary"
-        className={classes.submit}
-        component={Link} to="/login"
-
-      >
-        Login for Appointment Information
-       
-      </Button>
-     
-
-
-  </div>
-  <Box mt={8}>
-    <Copyright />
-  </Box>
-</Container>
-</div>
-
-
-
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              UF Department of Neurosurgery 
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              At the University of Florida Department of Neurosurgery our goal is to deliver Excellence in 
+              Patient Care, Excellence in Resident Education, and Excellence in Research.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button 
+                  type="submit"
+                  variant="contained" 
+                  color="secondary"
+                  component={Link} to="/login">
+                    Login for Appointment Information 
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                  type="submit"
+                  variant="contained"
+                  color="secondary"
+                  component={Link} to="/directions">
+                    Click for Directions
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
         </div>
-        </MuiThemeProvider>
-
-                    
-    );
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={2}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          UF Health
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+        UF Health is a collaboration of the University of Florida Health Science Center, Shands hospitals and other health care entities.
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+    </MuiThemeProvider>
+  );
 }
 
+
+
 export default Home;
+
+
+
+
+
+
+
+
+
