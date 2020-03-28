bfde0c10e45e9a504a4f7ad6c7fbf059e31a84fb
meganwolf <meganpwolf@gmail.com>
2020-03-28 19:22:52 -0400

    controller for sms

---
+++ server/controllers/smsController.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const mongoose = require('mongoose')
+const config = require('../config/config.js')
+
+const accountSid = config.texting.accountSid;
+const authToken = config.texting.authToken;
+const client = require('twilio')(accountSid, authToken);
+
+  exports.sendText = async (req, res) => {
+    try {
+      client.messages
+      .create({
+         body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+         from: '+13345819814',
+         to: '+15072501199'
+       })
+      .then(message => console.log(message.sid));
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
