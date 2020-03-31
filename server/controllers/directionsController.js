0bbbacd928d0f68dc8835b96d692d1f7d0e0e2d4
Rohil Tuli <rohil.tuli@gmail.com>
2020-03-31 14:03:56 -0400

    Fixed errors in backend

--- server/controllers/directionsController.js
+++ server/controllers/directionsController.js
@@ -14,10 +14,18 @@ exports.getDirections = async (req, res) => {
       const response = await axios.get('https://maps.googleapis.com/maps/api/directions/json?key=' + (process.env.MAP_KEY || require('../config/config.js').directions.key) + '&origin=' + origin + '&destination='+ destination);
       // console.log(response.data);
       // res.send(response.data);
-      res.send({
-        distance: response.data.routes[0].legs[0].distance.text,
-        duration: response.data.routes[0].legs[0].duration.text
-      });
+      if (!response.data.routes) {
+        res.send({
+          distance: "Invalid address",
+          duration: "Invalid address"
+        })
+      }
+      else {
+        res.send({
+          distance: response.data.routes[0].legs[0].distance.text,
+          duration: response.data.routes[0].legs[0].duration.text
+        });
+      }
     })
   }
   catch (err) {
