63172490d0943ff3a5f7d817fda3860583baec04
meganwolf <meganpwolf@gmail.com>
2020-04-19 23:52:37 -0400

    simple reply

--- server/controllers/smsController.js
+++ server/controllers/smsController.js
@@ -24,10 +24,10 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
   exports.receiveText = async (req, res) => {
     const twiml = new MessagingResponse();
-    const message = twiml.message();
-    message.body(req.body.from + ' ' + req.body.body);
-    message.media('https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg');
 
+    const message = twiml.message();
+    message.body('If you have any questions or concerns regarding your upcoming appointment with the UF Neurosurgery Department please call 352-273-9000.');
+  
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
   }
